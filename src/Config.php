e959c6c95a07a5a7d85e81851febf891aa215b8a
Naman Kumar <namankumar80510@gmail.com>
2023-08-05 12:55:31 +0530

    array of files

--- src/Config.php
+++ src/Config.php
@@ -6,29 +6,47 @@ use Symfony\Component\Yaml\Yaml;
 
 class Config
 {
+    private string|array $source;
 
-    private string $file;
-
-    public function __construct(string $file){
-        $this->file = $file;
+    public function __construct(string|array $source)
+    {
+        $this->source = $source;
     }
 
-    public function parse()
+    public function parse(): array
     {
-        return Yaml::parseFile($this->file);
+        $settings = [];
+
+        if (is_array($this->source)) {
+            // If the source is an array, treat it as an array of file paths
+            foreach ($this->source as $file) {
+                $config = Yaml::parseFile($file);
+                $settings = array_merge_recursive($settings, $config);
+            }
+        } else if (is_string($this->source)) {
+            // If the source is a string, treat it as a single file path
+            $settings = Yaml::parseFile($this->source);
+        } else {
+            throw new \InvalidArgumentException("Invalid source provided. It should be a folder path or an array of file paths.");
+        }
+
+        return $settings;
     }
 
     public function get(string $key): mixed
     {
         $settings = $this->parse();
 
-        $key = explode('.', $key);
+        $keys = explode('.', $key);
 
-        if (count($key) === 1) {
-            return $settings[$key[0]] ?? null;
+        $value = $settings;
+        foreach ($keys as $subKey) {
+            if (!isset($value[$subKey])) {
+                return null;
+            }
+            $value = $value[$subKey];
         }
 
-        return $settings[$key[0]][$key[1]] ?? null;
+        return $value;
     }
-
 }
